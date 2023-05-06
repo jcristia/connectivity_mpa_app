c6c3cce3c5ff46dd9b8edf7d613d59c93e2a85ec
John Cristiani <jcristia10@gmail.com>
2023-05-06 11:39:37 -0700

    archive demo

--- app.py
+++ app.py
@@ -1,43 +1,24 @@
-# https://docs.streamlit.io/library/get-started/create-an-app
 
-# to run this you can't just do "streamlit run app.py" from the conda environment
-# you have to run it as a python module:
+
+# to run from conda
 # python -m streamlit run app.py
 
 
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('MPA Network Connectivity')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+#@st.cache_data
+# geojson data
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+with st.sidebar.form(key="my_form"):
+    expander = st.sidebar.expander("Study description")
+    expander.write(
+        """
+    This app visualizes data from **PAPER TITLE AND LINK**
+    Blah blah blah
+    Blah blah blah
+    """
+    )
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
-
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+st.map(data=None, zoom=None, use_container_width=True)
